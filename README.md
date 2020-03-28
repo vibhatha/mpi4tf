56b02da32aa8a90cb6dec941d3c9e0c437a29e9e
vibhatha <vibhatha@gmail.com>
2020-03-28 14:51:02 -0400

    adding setup and readme update

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# tfpipegrad
+# mpi4tf
+
+This is a basic distributed training enabled platform for training Tensorflow. 
+
+## Pre-requisites
+
+Install OpenMPI 4.x.x or any other MPI implementation. 
+
+## Install via Pip
+
+```bash
+pip3 install mpi4tf
+```
+
+## Install From Source
+
+Clone the `mpi4tf` repo then, 
+
+```bash
+python3 setup.py install
+```
+
+## Development Mode
+
+In the development mode use the following command to build the libraries. 
+
+```bash
+python3 setup.py develop
+```
+
+## Test
+
+```bash
+mpirun -n 4 python3 test/test_mpi.py
+```
+
+## MNIST Data Parallel Demo
+
+Run with Parallelism 4
+
+```bash
+./bin/run_mnist_dist.sh 4
+```
+
+## Notes
+
+As this is a MPI backend you can use all the MPI flags to add different functionality
+in running experiments. 
--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 # Reference: https://github.com/audreyr/cookiecutter-pypackage
-#from distutils.core import setup
+# from distutils.core import setup
 from setuptools import setup
+
 setup(
     name='mpi4tf',
     packages=[],
     version='0.0.1',
     description='Tensorflow Based Distributed Training with MPI',
     author='Vibhatha Abeykoon',
-    license='Apache License 2.0',
     author_email='vibhatha@gmail.com',
     url='https://github.com/vibhatha/mpi4tf',
-    keywords=['tensorflow', 'parallelism', 'mpi' ],
+    keywords=['tensorflow', 'parallelism', 'mpi'],
     python_requires='>=3.6',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: Apache License 2.0',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
@@ -25,5 +24,10 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
+    ],
+    install_requires=[
+        'tensorflow',
+        'mpi4py',
+        'numpy',
     ],
 )
